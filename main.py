f897fe4ba0eedcf5fe72c84f08369fde9a65a95a
Jamshid <mamatovjamshid605@gmail.com>
2023-02-22 11:05:23 +0500

    check whether people visible or not

--- main.py
+++ main.py
@@ -34,8 +34,8 @@ def scrape():
     loginbutton.click()
 
     time.sleep(10)
-
-
-
+    driver.get('https://www.instagram.com/dostiyorov__m/following/')
+    WebDriverWait(driver,timeout=15).until(EC.presence_of_element_located((By.XPATH,'//span[@class="x193iq5w xeuugli x1fj9vlw x13faqbe x1vvkbs xt0psk2 x1i0vuye xvs91rp x1s688f x7l2uk3 x10wh9bi x1wdrske x8viiok x18hxmgj"]')))
+    
 if __name__=="__main__":
     scrape()
